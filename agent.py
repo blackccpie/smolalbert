c6e8420f4f1074cd97a152e3120b719981df39eb
blackccpie <16404298+blackccpie@users.noreply.github.com>
2025-09-13 10:42:37 +0200

    feat : starting to work on a custom ui.

--- agent.py
+++ agent.py
@@ -1,7 +1,31 @@
+# The MIT License
+
+# Copyright (c) 2025 Albert Murienne
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import os
 
-from smolagents import CodeAgent, GradioUI, InferenceClientModel, Tool
+from smolagents import CodeAgent, InferenceClientModel, Tool
 from tavily import TavilyClient
+
+from agent_ui import AgentUI
 
 # Define a custom tool for Tavily search
 from smolagents import Tool
@@ -61,5 +85,5 @@ agent = CodeAgent(
     )
 )
 
-gradio_ui = GradioUI(agent, file_upload_folder=None, reset_agent_memory=True)
-gradio_ui.launch(share=False)
+agent_ui = AgentUI(agent, file_upload_folder=None, reset_agent_memory=True)
+agent_ui.launch(share=False)
